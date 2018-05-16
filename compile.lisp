6ae5eb9305f215fbfe2dd40a4d29145542c3a7b1
BHa2R00 <32067941+BHa2R00@users.noreply.github.com>
2018-05-16 23:26:07 +0800

    Delete compile.lisp

--- compile.lisp
+++
@@ -1,8 +0,0 @@
-(require :cmp)
-(in-package :cl-user)
-(compile-file "sgk.lisp" :system-p t)
-(compile-file "vrom_compile.lisp" :system-p t)
-(c:build-program "vrom_compile" :lisp-files '(
-											  "sgk.o"
-											  "vrom_compile.o"
-											  ))
